2646f0363cde73b2e4bbaa305fdb54535cb7cdff
Arpine <a.aleksanyan@diagnocat.com>
2023-05-10 16:18:36 +0400

    refactor v4

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
 import "semantic-ui-css/semantic.min.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
